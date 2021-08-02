25767a5c4b7920acac923501ec801dd0cd40be42
sylvan008 <sylvan.climb@gmail.com>
2021-08-02 15:24:48 +0300

    Remove all views from 'film-board'

--- src/view/films-board.js
+++ src/view/films-board.js
@@ -1,12 +1,1 @@
-import {createFilmList} from './film-list.js';
-import {filmListExtra} from './film-list-extra.js';
-
-export const createFilmsBoard = (films) => (`
-  <section class="films">
-    ${createFilmList(films.allFilms)}
-
-    ${filmListExtra('Top rated', films.topRated)}
-
-    ${filmListExtra('Most commented', films.mostRecommended)}
-  </section>
-`);
+export const createFilmsBoard = () => '<section class="films"></section>';
