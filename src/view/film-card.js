bd73925cf2c29920800628a59f0038edb4a23136
sylvan008 <sylvan.climb@gmail.com>
2021-08-02 16:38:57 +0300

    Will move 'film-card-controls' to 'film-card'.

--- src/view/film-card.js
+++ src/view/film-card.js
@@ -1,5 +1,3 @@
-import {createFilmCardControls} from './film-card-controls.js';
-
 export const createFilmCard = () => `
   <article class="film-card">
     <h3 class="film-card__title">The Dance of Life</h3>
@@ -12,6 +10,11 @@ export const createFilmCard = () => `
     <img src="./images/posters/the-dance-of-life.jpg" alt="" class="film-card__poster">
     <p class="film-card__description">Burlesque comic Ralph "Skid" Johnson (Skelly), and specialty dancer Bonny Lee King (Carroll), end up together on a cold, rainy night at a tr…</p>
     <a class="film-card__comments">5 comments</a>
-    ${createFilmCardControls()}
+
+    <div class="film-card__controls">
+      <button class="film-card__controls-item film-card__controls-item--add-to-watchlist" type="button">Add to watchlist</button>
+      <button class="film-card__controls-item film-card__controls-item--mark-as-watched" type="button">Mark as watched</button>
+      <button class="film-card__controls-item film-card__controls-item--favorite" type="button">Mark as favorite</button>
+    </div>
   </article>
 `;
