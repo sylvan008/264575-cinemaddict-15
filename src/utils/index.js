0ad06a9f0e846fa7992d543a11f1f49792011329
sylvan008 <sylvan.climb@gmail.com>
2021-08-03 14:39:02 +0300

    Will add sorting function for films lists
    - sorting films by rating
    -sorting films by count of comments

--- src/utils/index.js
+++ src/utils/index.js
@@ -16,9 +16,23 @@ const getHumanizeDate = (date, template='D YYYY') => dayjs(date).format(template
 
 const getHumanizeFilmDuration = (runtime) => `${Math.floor(runtime / HOUR)}h ${runtime % HOUR}m`;
 
+const sortFilmByRating = (filmA, filmB) => {
+  const totalRatingA = filmA.filmInfo.totalRating;
+  const totalRatingB = filmB.filmInfo.totalRating;
+  return totalRatingB - totalRatingA;
+};
+
+const sortFilmByComments = (filmA, filmB) => {
+  const commentsCountA = filmA.comments.length;
+  const commentsCountB = filmB.comments.length;
+  return commentsCountB - commentsCountA;
+};
+
 export {
   getRandomInteger,
   getRandomArrayElement,
   getHumanizeDate,
-  getHumanizeFilmDuration
+  getHumanizeFilmDuration,
+  sortFilmByRating,
+  sortFilmByComments
 };
