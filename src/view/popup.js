9180959d83ddc205da5b9537766573a1b155c842
sylvan008 <sylvan.climb@gmail.com>
2021-08-03 17:12:30 +0300

    Will add filData to the template

--- src/view/popup.js
+++ src/view/popup.js
@@ -1,4 +1,60 @@
-export const createPopup = () => `
+import {getHumanizeDate, getHumanizeFilmDuration} from '../utils';
+import {FilmControlTypes} from '../utils/const.js';
+
+const CONTROL_ACTIVE_CLASS = 'film-details__control-button--active';
+const filmDetailControlTypes = [
+  {
+    classModifier: 'film-details__control-button--watchlist',
+    text: 'Add to watchlist',
+    type: FilmControlTypes.WATCHLIST,
+  },
+  {
+    classModifier: 'film-details__control-button--watched',
+    text: 'Already watched',
+    type: FilmControlTypes.WATCHED,
+  },
+  {
+    classModifier: 'film-details__control-button--favorite',
+    text: 'Add to favorites',
+    type: FilmControlTypes.FAVORITE,
+  },
+];
+const createGenreItem = (genre) => `<span class="film-details__genre">${genre}</span>`;
+const createControlButton = ({classModifier, text, type}, userDetails) => {
+  const activeClass = userDetails[type] ? CONTROL_ACTIVE_CLASS : '';
+  return `
+    <button type="button" class="film-details__control-button ${classModifier} ${activeClass}" id="${type}" name="${type}">
+      ${text}
+    </button>
+  `;
+};
+
+export const createPopup = ({filmInfo, userDetails}) => {
+  const {
+    poster,
+    title,
+    originalTitle,
+    totalRating,
+    ageRating,
+    director,
+    writers,
+    actors,
+    release,
+    runtime,
+    genre,
+    description,
+  } = filmInfo;
+
+  const writersStr = writers.join(', ');
+  const actorsStr = actors.join(', ');
+  const releaseDate = getHumanizeDate(release.date, 'D MMMM YYYY');
+  const releaseCountry = release.country;
+  const filmDuration = getHumanizeFilmDuration(runtime);
+  const genreTemplate = genre.map(createGenreItem).join('');
+  const filmDetailControlTemplate = filmDetailControlTypes
+    .map((type) => createControlButton(type, userDetails))
+    .join('');
+  return `
   <section class="film-details">
     <form class="film-details__inner" action="" method="get">
       <div class="film-details__top-container">
@@ -7,71 +63,63 @@ export const createPopup = () => `
         </div>
         <div class="film-details__info-wrap">
           <div class="film-details__poster">
-            <img class="film-details__poster-img" src="./images/posters/the-great-flamarion.jpg" alt="">
+            <img class="film-details__poster-img" src="${poster}" alt="">
 
-            <p class="film-details__age">18+</p>
+            <p class="film-details__age">${ageRating}</p>
           </div>
 
           <div class="film-details__info">
             <div class="film-details__info-head">
               <div class="film-details__title-wrap">
-                <h3 class="film-details__title">The Great Flamarion</h3>
-                <p class="film-details__title-original">Original: The Great Flamarion</p>
+                <h3 class="film-details__title">${title}</h3>
+                <p class="film-details__title-original">Original: ${originalTitle}</p>
               </div>
 
               <div class="film-details__rating">
-                <p class="film-details__total-rating">8.9</p>
+                <p class="film-details__total-rating">${totalRating}</p>
               </div>
             </div>
 
             <table class="film-details__table">
               <tr class="film-details__row">
                 <td class="film-details__term">Director</td>
-                <td class="film-details__cell">Anthony Mann</td>
+                <td class="film-details__cell">${director}</td>
               </tr>
               <tr class="film-details__row">
                 <td class="film-details__term">Writers</td>
-                <td class="film-details__cell">Anne Wigton, Heinz Herald, Richard Weil</td>
+                <td class="film-details__cell">${writersStr}</td>
               </tr>
               <tr class="film-details__row">
                 <td class="film-details__term">Actors</td>
-                <td class="film-details__cell">Erich von Stroheim, Mary Beth Hughes, Dan Duryea</td>
+                <td class="film-details__cell">${actorsStr}</td>
               </tr>
               <tr class="film-details__row">
                 <td class="film-details__term">Release Date</td>
-                <td class="film-details__cell">30 March 1945</td>
+                <td class="film-details__cell">${releaseDate}</td>
               </tr>
               <tr class="film-details__row">
                 <td class="film-details__term">Runtime</td>
-                <td class="film-details__cell">1h 18m</td>
+                <td class="film-details__cell">${filmDuration}</td>
               </tr>
               <tr class="film-details__row">
                 <td class="film-details__term">Country</td>
-                <td class="film-details__cell">USA</td>
+                <td class="film-details__cell">${releaseCountry}</td>
               </tr>
               <tr class="film-details__row">
                 <td class="film-details__term">Genres</td>
-                <td class="film-details__cell">
-                  <span class="film-details__genre">Drama</span>
-                  <span class="film-details__genre">Film-Noir</span>
-                  <span class="film-details__genre">Mystery</span></td>
+                <td class="film-details__cell">${genreTemplate}</td>
               </tr>
             </table>
 
-            <p class="film-details__film-description">
-              The film opens following a murder at a cabaret in Mexico City in 1936, and then presents the events leading up to it in flashback. The Great Flamarion (Erich von Stroheim) is an arrogant, friendless, and misogynous marksman who displays his trick gunshot act in the vaudeville circuit. His show features a beautiful assistant, Connie (Mary Beth Hughes) and her drunken husband Al (Dan Duryea), Flamarion's other assistant. Flamarion falls in love with Connie, the movie's femme fatale, and is soon manipulated by her into killing her no good husband during one of their acts.
-            </p>
+            <p class="film-details__film-description">${description}</p>
           </div>
         </div>
 
-        <section class="film-details__controls">
-          <button type="button" class="film-details__control-button film-details__control-button--watchlist" id="watchlist" name="watchlist">Add to watchlist</button>
-          <button type="button" class="film-details__control-button film-details__control-button--active film-details__control-button--watched" id="watched" name="watched">Already watched</button>
-          <button type="button" class="film-details__control-button film-details__control-button--favorite" id="favorite" name="favorite">Add to favorites</button>
-        </section>
+        <section class="film-details__controls">${filmDetailControlTemplate}</section>
       </div>
 
       <div class="film-details__bottom-container"></div>
     </form>
   </section>
 `;
+};
