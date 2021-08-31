608ac196ecf3ad6e271c10dc44ddac20d1ffa9fc
sylvan008 <sylvan.climb@gmail.com>
2021-08-31 16:32:41 +0300

    implements smart component and auto-update of the comment form

--- src/view/new-comment.js
+++ src/view/new-comment.js
@@ -1,5 +1,10 @@
-import AbstractComponent from '../abstract-component.js';
+import SmartComponent from '../smart-component.js';
 import {emotions} from '../utils/const.js';
+
+const CallbackTypes = {
+  SUBMIT: 'submit',
+  CHANGE: 'change',
+};
 
 const createEmojiItem = (emoji) =>
   `<input class="film-details__emoji-item visually-hidden" name="comment-emoji" type="radio" id="emoji-${emoji}" value="${emoji}">
@@ -8,12 +13,14 @@ const createEmojiItem = (emoji) =>
   </label>
 `;
 
-const createNewCommentTemplate = () =>
+const createNewCommentTemplate = ({emotion, comment, isEmotion}) =>
   `<div class="film-details__new-comment">
-    <div class="film-details__add-emoji-label"></div>
+    <div class="film-details__add-emoji-label">
+      ${isEmotion ? `<img src="images/emoji/${emotion}.png" width="55" height="55" alt="emoji-${emotion}">` : ''}
+    </div>
 
     <label class="film-details__comment-label">
-      <textarea class="film-details__comment-input" placeholder="Select reaction below and write comment here" name="comment"></textarea>
+      <textarea class="film-details__comment-input" placeholder="Select reaction below and write comment here" name="comment">${comment}</textarea>
     </label>
 
     <div class="film-details__emoji-list">
@@ -22,11 +29,77 @@ const createNewCommentTemplate = () =>
   </div>
 `;
 
-export default class NewComment extends AbstractComponent {
+export default class NewComment extends SmartComponent {
+  constructor() {
+    super();
+    this._data = NewComment.parseFormToData({
+      emotion: null,
+      comment: '',
+    });
+
+    this._emotionChangeHandler = this._emotionChangeHandler.bind(this);
+    this._commentInputHandler = this._commentInputHandler.bind(this);
+    this._setInnerHandlers();
+  }
+
+  static parseFormToData(form) {
+    return Object.assign({}, form, {
+      isEmotion: !!form.emotion,
+    });
+  }
+
+  static parseDataToForm(data) {
+    data = Object.assign({}, data);
+    delete data.isEmotion;
+    return data;
+  }
+
   /**
    * @return {string}
    */
   getTemplate() {
-    return createNewCommentTemplate();
+    return createNewCommentTemplate(NewComment.parseFormToData(this._data));
+  }
+
+  setFormSubmitHandler(callback) {
+    this._callback[CallbackTypes.SUBMIT] = callback;
+    document.querySelector('.film-details__inner')
+      .addEventListener(CallbackTypes.SUBMIT, this._submitFormHandler);
+  }
+
+  _emotionChangeHandler(evt) {
+    evt.preventDefault();
+    if (!evt.target.matches('.film-details__emoji-item')) {
+      return;
+    }
+    this.updateData({
+      emotion: evt.target.value.trim(),
+    });
+  }
+
+  _commentInputHandler(evt) {
+    evt.preventDefault();
+    this.updateData({
+      comment: evt.target.value,
+    },true);
+  }
+
+  restoreHandlers() {
+    this._setInnerHandlers();
+    this.setFormSubmitHandler(this._callback[CallbackTypes.SUBMIT]);
+  }
+
+  _setInnerHandlers() {
+    this.getElement()
+      .querySelector('.film-details__emoji-list')
+      .addEventListener('change', this._emotionChangeHandler);
+    this.getElement()
+      .querySelector('.film-details__comment-input')
+      .addEventListener('input', this._commentInputHandler);
+  }
+
+  _submitFormHandler(evt) {
+    evt.preventDefault();
+    this._callback[CallbackTypes.SUBMIT](NewComment.parseFormToData(this._data));
   }
 }
