6e42189533e45c08f1c384798d01a5930e00d9ca
sylvan008 <sylvan.climb@gmail.com>
2021-09-15 13:57:19 +0300

    Создаст класс для работы с API

---
+++ src/api.js
@@ -0,0 +1,62 @@
+const Method = {
+  GET: 'GET',
+  PUT: 'PUT',
+};
+
+const SuccessHTTPStatusRange = {
+  MIN: 200,
+  MAX: 299,
+};
+
+export default class Api {
+  constructor(endpoint, authorization) {
+    this._endpoint = endpoint;
+    this._authorization = authorization;
+  }
+
+  getFilms() {
+    return this._load({url: 'movies'})
+      .then(Api.toJSON);
+  }
+
+  updateFilm(film) {
+    return this._load({
+      url: `/movies/${film.filmInfo.id}`,
+      method: Method.PUT,
+      body: JSON.stringify(film),
+      headers: new Headers({'Content-Type': 'application/json'}),
+    })
+      .then(Api.toJSON);
+  }
+
+  _load({
+    url,
+    method = Method.GET,
+    body = null,
+    headers = new Headers(),
+  }) {
+    headers.append('Authorization', this._authorization);
+
+    return fetch(
+      `${this._endpoint}/${url}`,
+      {method, body, headers},
+    )
+      .then(Api.checkStatus)
+      .catch(Api.catchError);
+  }
+
+  static toJSON(response) {
+    return response.json();
+  }
+
+  static checkStatus(response) {
+    if (response.status < SuccessHTTPStatusRange.MIN || response.status > SuccessHTTPStatusRange.MAX) {
+      throw new Error(`${response.status}: ${response.statusText}`);
+    }
+    return response;
+  }
+
+  static catchError(error) {
+    throw error;
+  }
+}
